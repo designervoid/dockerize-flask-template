19fea66ad140fc298030f3563d2cf264aa43afee
designervoid <newluciocheg@gmail.com>
2020-01-07 20:00:02 +0300

    hotfix

--- app/views.py
+++ app/views.py
@@ -1,33 +1,11 @@
 from flask import jsonify, request, send_file
 from app import app
 from app.random_string import RandomString
-from app.generate import generate 
-
-
-# RANDOM STRING
-class RandomString():
-    def __init__(self, string_length):
-        self.__string_length = string_length
-
-    def get_image_name(self):
-        return self.__image_name
-
-    def set_image_name(self, image_name):
-        self.__image_name = image_name
-
-    def generate_random_string(self):
-        """Generate a random string of fixed length """
-        letters = string.ascii_lowercase
-        image_name = ''.join(random.choice(letters) for i in range(self.__string_length))
-        self.set_image_name(image_name)
-        return image_name
+from app.generate import generate
 
 obj_random_string = RandomString(20)
 obj_random_string.generate_random_string()
 image_name = obj_random_string.get_image_name()
-
-
-# API
 
 @app.route("/api/generate", methods=["POST"])
 def generate_text():
